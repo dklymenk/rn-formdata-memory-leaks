f125a10f10b567b41c2926d30216dfeb1d64f95b
Dmytro Klymenko <klimenkodr@gmail.com>
2022-06-11 14:33:02 +0300

    add file upload

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  ActivityIndicator,
   Button,
   SafeAreaView,
   ScrollView,
@@ -7,14 +8,39 @@ import {
   Text,
   View,
 } from 'react-native';
+import axios from 'axios';
 
 const App = () => {
+  const [loading, setLoading] = React.useState(false);
+  const handleUpload = async () => {
+    setLoading(true);
+    console.log('Uploading...');
+    const form = new FormData();
+    form.append('file', {
+      uri: 'https://freetestdata.com/wp-content/uploads/2022/02/Free_Test_Data_15MB_MP4.mp4',
+      name: 'Free_Test_Data_15MB_MP4.mp4',
+      type: 'video/mp4',
+    });
+    const headers = {
+      'Content-Type': 'multipart/form-data',
+    };
+    try {
+      const {status, data} = await axios.post('https://0x0.st', form, {
+        headers,
+      });
+      console.log({status, data});
+    } catch (error) {
+      console.error(error);
+    }
+    setLoading(false);
+  };
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
         <View style={styles.content}>
           <Text style={styles.heading}>Axios form data memory leak demo</Text>
-          <Button title="Upload" onPress={() => console.log('Upload')} />
+          <Button disabled={loading} title="Upload" onPress={handleUpload} />
+          <ActivityIndicator animating={loading} />
         </View>
       </ScrollView>
     </SafeAreaView>
