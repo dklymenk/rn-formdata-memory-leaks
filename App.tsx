c93137e1b7f33094747ca21d6563ffd77537e594
Dmytro Klymenko <klimenkodr@gmail.com>
2022-06-11 20:57:25 +0300

    provide fetch and axios options in UI

--- App.tsx
+++ App.tsx
@@ -31,51 +31,56 @@ const files: File[] = [
   },
 ];
 
-const File: React.FC<{file: File}> = ({file}) => {
+const url = 'https://0x0.st';
+
+const headers = {
+  'Content-Type': 'multipart/form-data',
+};
+
+const fetchUploader = async (form: FormData) => {
+  const response = await fetch(url, {
+    headers: new Headers(headers),
+    method: 'POST',
+    body: form,
+  }).then(res => res.text());
+  return response;
+};
+
+const axiosUploader = async (form: FormData) => {
+  const {data} = await axios.post(url, form, {
+    headers,
+  });
+  return data;
+};
+
+const uploaders = {fetchUploader, axiosUploader};
+
+const File: React.FC<{
+  file: File;
+  uploader: (form: FormData) => Promise<string>;
+  name: string;
+}> = ({file, uploader, name}) => {
   const [isLoading, setIsLoading] = React.useState(false);
-
-  const headers = {
-    'Content-Type': 'multipart/form-data',
-  };
-
-  const fetchUpload = async (form: FormData) => {
-    try {
-      const response = await fetch('https://0x0.st', {
-        headers: new Headers(headers),
-        method: 'POST',
-        body: form,
-      }).then(res => res.text());
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const axiosUpload = async (form: FormData) => {
-    try {
-      const {data} = await axios.post('https://0x0.st', form, {
-        headers,
-      });
-      console.log(data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
 
   const handleUpload = async () => {
     setIsLoading(true);
     console.log('Uploading...');
     const form = new FormData();
     form.append('file', file);
-    // await fetchUpload(form);
-    await axiosUpload(form);
+
+    try {
+      const response = await uploader(form);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
     setIsLoading(false);
   };
 
   return (
     <View style={styles.file}>
       <Text style={styles.text}>{file.size}</Text>
-      <Button disabled={isLoading} title="Upload" onPress={handleUpload} />
+      <Button disabled={isLoading} title={name} onPress={handleUpload} />
       <ActivityIndicator animating={isLoading} />
     </View>
   );
@@ -85,10 +90,17 @@ const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
-        <Text style={styles.heading}>Axios form data memory leak demo</Text>
-        {files.map((file, index) => (
-          <File key={index} file={file} />
-        ))}
+        <Text style={styles.heading}>Form data memory leak demo</Text>
+        {(Object.keys(uploaders) as (keyof typeof uploaders)[]).map(key =>
+          files.map((file, index) => (
+            <File
+              key={`axios-${index}`}
+              file={file}
+              name={key}
+              uploader={uploaders[key]}
+            />
+          )),
+        )}
       </View>
     </SafeAreaView>
   );
